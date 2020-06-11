b5e1975982d78aec9cd9de1c73253413d418c78d
Marco Correa <marcoafc86@gmail.com>
2020-06-11 16:55:24 -0300

    ajuste link

--- project.js
+++ project.js
@@ -7,14 +7,15 @@
   head.innerHTML = head.innerHTML + ('<link rel="stylesheet" href="https://mylinkisyourlink.herokuapp.com/assets/styles.css"></link>');
   head.innerHTML = head.innerHTML + ('<link rel="stylesheet" href="https://use.typekit.net/aap5wjp.css"></link>');
   
+  var widget = document.querySelector('#mliyl-project');
+  var linkPortfolio = widget.dataset.linkPortfolio;
+  
   var languages = {
     pt: "⚠ <span style='text-decoration: line-through;'>Obrigado por acessar meu portfolio</span>. Eu doei meus acessos para o site de um profissional que está sem trabalho graças à crise do Covid-⓵⑨. e, nesse ℳomento, precisa mais do que eu. Quem sabe você não ©ontrata el☒? <a target='_blank' href='" + linkPortfolio + "'>☞ Clica aqui e vai na fé, que essx criativ☆ tem um puta portfolio.</a>",
     en: "⚠ <span style='text-decoration: line-through;'> Thanks for popping by</span>. I donated my portfolio's views to the site of a creative who's lost his job because of the covid-⓵⑨ crisis. During these tough times, they need ℳore than just my help. Who knows you could hire them. <a target='_blank' href='" + linkPortfolio + "'>☞ Click here and go for it. Their portfolio is also very g☆od.</a>",
     es: "⚠ <span style='text-decoration: line-through;'>Obrigado por acessar meu portfolio</span>. Eu doei meus acessos para o site de um profissional que está sem trabalho graças à crise do Covid-⓵⑨. e, nesse ℳomento, precisa mais do que eu. Quem sabe você não ©ontrata el☒? <a target='_blank' href='" + linkPortfolio + "'>☞ Clica aqui e vai na fé, que essx criativ☆ tem um puta portfolio.</a>"
   }
   
-  var widget = document.querySelector('#mliyl-project');
-  var linkPortfolio = widget.dataset.linkPortfolio;  
   var textSelected = languages[widget.dataset.language] || languages.pt;
 
   widget.innerHTML = ""
